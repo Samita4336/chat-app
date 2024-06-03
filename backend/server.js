1a613ade53d95d6548e4f137bd19843049e19ff6
Samuel <samuel@eaglelionsystems.com>
2024-06-03 08:46:16 -0400

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -6,11 +7,14 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectToMongoDb from './db/connectToMongoDB.js';
-
-const app = express();
-const PORT = process.env.PORT || 5000;
+import { app, server } from './socket/socket.js';
 
 dotenv.config();
+
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json()); // to parse the incoming requests with JSON payload (from req.body)
 app.use(cookieParser()); 
@@ -19,12 +23,18 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 // app.get('/', (req, res) => {
 //     root  route http://localhost:5000/
 //   res.send('Hello world!');
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDb();
     console.log(`server running on port ${PORT}`)
 });
