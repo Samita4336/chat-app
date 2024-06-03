f7e6132c8997ebe3039b6c12602a086489d519e4
Samuel <samuel@eaglelionsystems.com>
2024-06-03 09:06:08 -0400

    bug fix

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -22,8 +22,8 @@ export const signup = async (req, res) => {
 
        // https://avatar-placeholder.iran.liara.run/
 
-       const girlProfilePic = `https://avatar.iran.liara.run/public/girl${username}`
-       const boyProfilePic = `https://avatar.iran.liara.run/public/boy${username}`
+       const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+       const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
 
        const newUser = new User({
            fullName,
